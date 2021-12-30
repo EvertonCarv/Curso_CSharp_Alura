5dea11d667624d7937c7181a2a248b7bd7fa01c4
Everton carvalho <everton.de.carvalho@hotmail.com.br>
2021-12-30 17:49:00 -0300

    Correção For encadeado

--- P13_For_Encadeado/Program.cs
+++ P13_For_Encadeado/Program.cs
@@ -5,7 +5,7 @@ for (int contadorLinha = 0; contadorLinha < 10; contadorLinha++)
 {
     for (int contadorColuna = 0; contadorColuna < 10; contadorColuna++)
     {
-        Console.WriteLine("*");
+        Console.Write("*");
        if (contadorColuna >= contadorLinha)
           break;
     }
